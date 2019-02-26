69d448034eb5962c76d6670229148b97030cf464
Twan Natter <twan180401@gmail.com>
2019-02-26 10:28:42 +0100

    Add project files.

---
+++ Top 2000/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Top_2000.Startup))]
+namespace Top_2000
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
